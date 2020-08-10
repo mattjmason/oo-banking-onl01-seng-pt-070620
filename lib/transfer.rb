fc7c1b122bc5109fe6b839bd2ebd275ce4811f2c
Matt Mason <masonmj10@gmail.com>
2020-08-10 04:23:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ def valid?
               if sender.balance < amount
                 @status = "rejected"
                 return "Transaction rejected. Please check your account balance."
-              elsif @status != "complete" && sender.valid?
+              elsif @status == "complete" && sender.valid?
                 @receiver.balance = @receiver.balance + amount
                 @sender.balance = @sender.balance - amount
                 @status = "complete"
